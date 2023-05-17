cab97cfc5409c40608592379dcc67a530027e9a3
JacobZufall <jacobzufall@gmail.com>
2023-05-16 20:41:32 -0400

    Refactored file structure.

---
+++ src/pya_jacobzufall/depreciation.py
@@ -0,0 +1,5 @@
+def sl_depr(cost: float, salvage: float, life: float) -> float:
+    return (cost - salvage) / life
+
+def syd_depr(cost: float, salvage: float, life: float) -> float:
+    pass
--- src/pya_jacobzufall/qbi.py
+++ src/pya_jacobzufall/qbi.py
@@ -1,12 +1,15 @@
-# Versioning for this file is formatted by date.
-# YYYY.MM.DD.v
-# The last number is for if there are multiple versions in a single day.
-# For example, a release on May 15th, 2023 would be "2023.05.15.1", assuming it's the first release of the day.
+"""
+Versioning for this file is formatted by date.
+YYYY.MM.DD.v
+The last number is for if there are multiple versions in a single day.
+For example, a release on May 15th, 2023 would be "2023.05.15.1", assuming it's the first release of the day.
 
-# These numbers are based on the numbers issued by the IRS yearly, and are expected to change. When these numbers
-# change, they will be updated as soon as possible. Currently planning on providing a way to manually override
-# these numbers easily, in case the update isn't quick enough for some people. Ideally, we should be able to
-# automatically pull these numbers from the web, but that's not even planned yet.
+These numbers are based on the numbers issued by the IRS yearly, and are expected to change. When these numbers
+change, they will be updated as soon as possible. Currently planning on providing a way to manually override
+these numbers easily, in case the update isn't quick enough for some people. Ideally, we should be able to
+automatically pull these numbers from the web, but that's not even planned yet.
+"""
+
 tax_limits: dict = {
     "single": {
         "lower": 170_050,
@@ -23,7 +26,8 @@ tax_limits: dict = {
 
 
 def calc_w2_limit(wages: float, ubia: float = 0.0) -> float:
-    num_one: float = wages * 0.5  # Will always get returned if there's no argument for UBIA.
+    # Will always get returned if there's no argument for UBIA.
+    num_one: float = wages * 0.5
     num_two: float = (wages * 0.25) + (ubia * 0.025)
 
     return max(num_one, num_two)
@@ -38,17 +42,17 @@ def calc_tn_qbi(qbi_amt: float) -> float:
 
 
 def calc_phase_in_per(tax_inc: float, f_status: str) -> float | None:
-    if f_status.lower() == "s":
+    if f_status.upper() == "S":
         return (tax_inc - tax_limits["single"]["lower"]) / tax_limits["single"]["phase_in"]
-    elif f_status.lower() == "mfj":
+    elif f_status.upper() == "MFJ":
         return (tax_inc - tax_limits["married"]["lower"]) / tax_limits["married"]["phase_in"]
 
 
 def calc_cat_one(tax_inc: float, qbi_amt: float, net_cap_gain: float, b_type: str) -> float | None:
     tn_qbi: float = calc_tn_qbi(qbi_amt)
-    if b_type.lower() == "qtb":
+    if b_type.upper() == "QTB":
         return tn_qbi
-    elif b_type.lower() == "sstb":
+    elif b_type.upper() == "SSTB":
         limit: float = (tax_inc - net_cap_gain) * 0.2
         if limit < tn_qbi:
             return limit
@@ -57,7 +61,7 @@ def calc_cat_one(tax_inc: float, qbi_amt: float, net_cap_gain: float, b_type: st
 
 
 def calc_cat_three(tax_inc: float, qbi_amt: float, w2_wages: float, f_status: str, b_type: str) -> float | None:
-    if b_type.lower() == "qtb":
+    if b_type.upper() == "QTB":
         tn_qbi: float = calc_tn_qbi(qbi_amt)
         w2_limit: float = calc_w2_limit(w2_wages)
         overall_limit: float = calc_tn_qbi(tax_inc)
@@ -70,7 +74,7 @@ def calc_cat_three(tax_inc: float, qbi_amt: float, w2_wages: float, f_status: st
 
         return min(red_qbi, w2_limit, overall_limit)
 
-    elif b_type.lower() == "sstb":
+    elif b_type.upper() == "SSTB":
         phase_in_per: float = calc_phase_in_per(tax_inc, f_status)
         applied_per: float = 1.0 - phase_in_per
 
@@ -86,15 +90,16 @@ def calc_cat_three(tax_inc: float, qbi_amt: float, w2_wages: float, f_status: st
 
 
 def calc_cat_two(tax_inc: float, qbi_amt: float, w2_wages: float, ubia: float, b_type: str) -> float | None:
-    if b_type.lower() == "qtb":
+    if b_type.upper() == "QTB":
         tn_qbi: float = calc_tn_qbi(qbi_amt)
         w2_limit: float = calc_w2_limit(w2_wages, ubia)
         overall_limit: float = calc_tn_qbi(tax_inc)
 
         return min(tn_qbi, w2_limit, overall_limit)
 
-    elif b_type.lower() == "sstb":
-        return 0.0  # There is no QBI deduction allowed for an SSTB for Category 2.
+    elif b_type.upper() == "SSTB":
+        # There is no QBI deduction allowed for an SSTB for Category 2.
+        return 0.0
 
 
 def qbi(filing_status: str,
@@ -106,7 +111,7 @@ def qbi(filing_status: str,
         ubia: float = 0.0) -> float:
     # For taxpayers with a filing status of Single or Head of Household.
     # Married Filing Single is included in here, since it's treated the same as filing single.
-    if filing_status.lower() == "s" or filing_status.lower == "hoh" or filing_status.lower() == "mfs":
+    if filing_status.upper() == "S" or filing_status.upper() == "HOH" or filing_status.upper() == "MFS":
         if taxable_income <= tax_limits["single"]["lower"]:
             # Category 1
             return calc_cat_one(taxable_income, qbi_amt, net_capital_gains, b_type)
@@ -120,7 +125,7 @@ def qbi(filing_status: str,
             return 0.0  # Returns 0 if they enter an invalid filing status.
 
     # For taxpayers with a filing status of Married Filing Jointly.
-    elif filing_status.lower() == "mfj":
+    elif filing_status.upper() == "MFJ":
         if taxable_income <= tax_limits["married"]["lower"]:
             return calc_cat_one(taxable_income, qbi_amt, net_capital_gains, b_type)
         elif tax_limits["married"]["lower"] < taxable_income < tax_limits["married"]["upper"]:
