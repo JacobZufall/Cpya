52a34f064f32181ac0b3be41efbadf8ccf8f0914
JacobZufall <jacobzufall@gmail.com>
2023-06-12 10:21:12 -0400

    Removed depreciation.py as it is now a method of TangibleAsset

--- src/cpya_jacobzufall/assets/asset.py
+++ src/cpya_jacobzufall/assets/asset.py
@@ -1,7 +1,16 @@
-# asset.py
+"""
+asset.py
+"""
+
 
 class Asset:
-    def __init__(self, name: str, life: float, value: float):
+    def __init__(self, name: str, life: int, value: float):
+        """
+
+        :param name: The name of the asset.
+        :param life: The life of the asset (months).
+        :param value: The value of the asset ($).
+        """
         self.name = name
         self.life = life
         self.value = value
--- src/cpya_jacobzufall/assets/intangible_asset.py
+++ src/cpya_jacobzufall/assets/intangible_asset.py
@@ -1,11 +1,25 @@
-# intangible_asset.py
+"""
+intangible_asset.py
+"""
 
 from asset import Asset
 
 
+# Might want to consider using months instead of years.
 class IntangibleAsset(Asset):
-    def __init__(self, name: str, life: float, value: float):
+    def __init__(self, name: str, life: int, value: float):
+        """
+
+        :param name: The name of the asset.
+        :param life: The life of the asset (months)).
+        :param value: The value of the asset.
+        """
         super().__init__(name=name, life=life, value=value)
 
-    def amortize(self):
-        pass
+    def amortize(self, periods: int = 1):
+        """
+
+        :param periods: The number of periods (usually years) to depreciate.
+        :return: Nothing.
+        """
+        self.value -= periods * (self.value / self.life)
