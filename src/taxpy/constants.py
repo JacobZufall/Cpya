abd916d5ed99bd36fb5f88fcbcf4f41cf2d09860
Jadon <jadonzufall@gmail.com>
2023-06-18 03:59:24 -0400

    some more minor tweeks

---
+++ src/taxpy/constants.py
@@ -0,0 +1,18 @@
+# Something like this is standard for implementing constant variables like this in a package.
+# this makes it easier to change values throughout the program.
+CREDIT = "credit"
+DEBIT = "debit"
+BALANCE_TYPES = [
+    CREDIT, 
+    DEBIT,
+]
+
+ASSET = "asset"
+LIABILITY = "liability"
+EQUITY = "equity"
+BAL_CATEGORYS = [
+    ASSET,
+    LIABILITY,
+    EQUITY,
+]
+
