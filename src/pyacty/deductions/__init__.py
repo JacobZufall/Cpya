40ba2da0b58f2530d6baea19d20e95e65b7d86ac
Jacob Zufall <jacobzufall@gmail.com>
2024-11-18 21:00:51 -0500

    Added disclaimer

--- src/pyacty/deductions/__init__.py
+++ src/pyacty/deductions/__init__.py
@@ -1,3 +1,6 @@
-from .Qbi import *
-from .QbiRange import *
-from .StandardDeduction import *
+"""
+Qbi.py, QbiRange.py, and StandardDeduction.py were the first files I wrote that became this entire project, so they're
+    very bad and don't really fit in with the rest of the project currently.
+
+They can still be accessed, but I'm not importing them in this __init__ file since they're not great.
+"""
